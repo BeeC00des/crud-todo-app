3bd3e1c368600fb351fa5bae8863e882abd75f03
Ajayi bolarinwa <ajayibcd@gmail.com>
2022-05-27 06:27:27 -0700

    adding edit and delete logic

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,7 +1,8 @@
 import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
 import { Component, OnInit } from '@angular/core';
 import {FormGroup, FormBuilder, Validators} from "@angular/forms"
-
+import { iTask } from '../model/task';
+ 
 
 @Component({
   selector: 'app-todo',
@@ -11,9 +12,13 @@ import {FormGroup, FormBuilder, Validators} from "@angular/forms"
 export class TodoComponent implements OnInit {
 
   todoForm !: FormGroup;
-  task: any[] = [];
-  taskProgress: any[] = [];
-  taskDone: any[] = [];
+  tasks: iTask[] = [];
+  taskProgress: iTask[] = [];
+  taskDone: iTask[] = [];
+  updateIndex!: any;
+  isEditEnabled: boolean = false;
+
+
 
   constructor(private formb: FormBuilder ) { }
 
@@ -23,7 +28,45 @@ export class TodoComponent implements OnInit {
     });
   }
 
-  drop(event: CdkDragDrop<string[]>) {
+  addTask(){
+    this.tasks.push({
+      description: this.todoForm.value.item,
+      done:false
+    })
+
+    this.todoForm.reset();
+  }
+
+  deleteTask(i:number){
+    this.tasks.splice(i , 1)
+  }
+
+  deleteTaskProgress(i:number){
+    this.taskProgress.splice(i , 1)
+  }
+
+  deleteTaskDone(i:number){
+    this.taskDone.splice(i , 1)
+  }
+
+
+  editTask(item:iTask, i:number){
+    this.todoForm.controls['item'].setValue(item.description);
+    this.updateIndex = i;
+    this.isEditEnabled = true;
+
+  }
+
+  updateTask(){
+    this.tasks[this.updateIndex].description = this.todoForm.value.item;
+    this.tasks[this.updateIndex].done = false;
+    this.todoForm.reset();
+    this.updateIndex = undefined;
+    this.isEditEnabled = false;
+  }
+
+
+  drop(event: CdkDragDrop<iTask[]>) {
     if (event.previousContainer === event.container) {
       moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
     } else {
