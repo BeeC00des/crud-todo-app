d5cc590be5a20f268f91be3b34564aa81ecaeb11
Ajayi bolarinwa <ajayibcd@gmail.com>
2022-05-27 04:08:45 -0700

    set up ui and basic functionality

---
+++ src/app/todo/todo.component.ts
@@ -0,0 +1,39 @@
+import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
+import { Component, OnInit } from '@angular/core';
+import {FormGroup, FormBuilder, Validators} from "@angular/forms"
+
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.scss']
+})
+export class TodoComponent implements OnInit {
+
+  todoForm !: FormGroup;
+  task: any[] = [];
+  taskProgress: any[] = [];
+  taskDone: any[] = [];
+
+  constructor(private formb: FormBuilder ) { }
+
+  ngOnInit(): void {
+    this.todoForm = this.formb.group({
+      item : ['', Validators.required]
+    });
+  }
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+  }
+
+}
